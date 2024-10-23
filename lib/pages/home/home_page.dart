146c9254880aac8796ae3f791450c3ffb4503188
Pramudya <pramudyap333@gmail.com>
2024-10-23 22:10:27 +0700

    update layout homepage

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:potato_apps/theme.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +9,58 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Widget header() {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20)),
+          color: primaryColor),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Selamat Datang',
+                style: whiteTextStyle,
+              ),
+              Text(
+                'Ilham Islamy',
+                style: whiteTextStyle.copyWith(fontSize: 24, fontWeight: bold),
+              ),
+              Text(
+                '30 Oktober, 2024',
+                style: whiteTextStyle,
+              ),
+            ],
+          )),
+          ClipOval(
+              child: Image.asset(
+            'assets/img_sample.jpeg',
+            width: 50,
+            height: 50,
+            fit: BoxFit.cover,
+          ))
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('HomePage'),);
+    return Expanded(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [header()],
+        ),
+      ),
+    );
   }
 }
