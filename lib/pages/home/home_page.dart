6137733a2b3abe76c14ea73749e9effb6c60e155
Pramudya <pramudyap333@gmail.com>
2024-10-27 19:39:27 +0700

    update layout homepage

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:potato_apps/theme.dart';
 import 'package:potato_apps/widget/header_home.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -139,28 +142,129 @@ class _HomePageState extends State<HomePage> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Expanded(
-              flex: 5,
-              child: Container(
-                width: double.infinity,
-                height: 158,
-                decoration: BoxDecoration(
-                    color: backgroundColor4,
-                    borderRadius: BorderRadius.circular(20)),
-              )),
+            flex: 5,
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              width: double.infinity,
+              height: 158,
+              decoration: BoxDecoration(
+                  color: fifthColor,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: const [
+                    BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 4,
+                        offset: Offset(2, 6))
+                  ]),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Temperature (Celcius)',
+                    style: blackTextStyle.copyWith(
+                        fontWeight: medium, fontSize: 12),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        '25\u00B0',
+                        style: primaryGreenTextStyle.copyWith(
+                            fontSize: 60, fontWeight: bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
           SizedBox(
             width: 12,
           ),
           Expanded(
               flex: 5,
               child: Container(
-                width: double.infinity,
-                height: 158,
-                decoration: BoxDecoration(
-                    color: backgroundColor4,
-                    borderRadius: BorderRadius.circular(20)),
-              )),
+                  width: double.infinity,
+                  height: 158,
+                  decoration: BoxDecoration(
+                      color: fifthColor,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 4,
+                            offset: Offset(2, 6))
+                      ]),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      // Radial Bar Chart
+                      SfCircularChart(
+                        series: <CircularSeries>[
+                          RadialBarSeries<ChartData, String>(
+                            dataSource: chartData,
+                            xValueMapper: (ChartData data, _) => data.x,
+                            yValueMapper: (ChartData data, _) => data.y,
+                            maximumValue: 100, // Maksimum 100% untuk kelembaban
+                            radius: '90%', // Mengatur radius
+                            innerRadius: '72%',
+                            cornerStyle: CornerStyle.bothCurve,
+                            trackColor: Colors.blueGrey,
+                            pointColorMapper: (ChartData data, _) =>
+                                Colors.orange,
+                            dataLabelSettings: DataLabelSettings(
+                              isVisible: false, // Sembunyikan label default
+                            ),
+                          ),
+                        ],
+                      ),
+                      // Teks di tengah lingkaran
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Humidity',
+                            style: blackTextStyle.copyWith(
+                                fontSize: 12, fontWeight: medium),
+                          ),
+                          Text(
+                              '${chartData[0].y.toInt()}%', // Menampilkan nilai kelembaban sebagai persentase
+                              style: primaryGreenTextStyle.copyWith(
+                                  fontSize: 28, fontWeight: bold)),
+                        ],
+                      ),
+                    ],
+                  ))),
         ],
       ),
+    );
+  }
+
+  Widget saveButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: 30, bottom: 30),
+          height: 36,
+          width: 100,
+          child: TextButton(
+              onPressed: () {
+                Fluttertoast.showToast(msg: 'Button Save Clicked');
+              },
+              style: TextButton.styleFrom(
+                  padding: EdgeInsets.zero,
+                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  backgroundColor: primaryColor,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8))),
+              child: Text(
+                'save',
+                style: whiteTextStyle.copyWith(fontSize: 18, fontWeight: bold),
+              )),
+        ),
+      ],
     );
   }
 
@@ -183,7 +287,16 @@ class _HomePageState extends State<HomePage> {
     // automation initialize
     automateValue = false;
     automateIndicator = automateValue ? "ON" : "OFF";
+
+    valueHumidity = 80;
+    chartData = [
+      ChartData('Humidity', valueHumidity), // 50% humidity
+    ];
   }
+
+  // Data kelembaban dengan 50% sebagai contoh
+  late double valueHumidity;
+  late List<ChartData> chartData;
 
   @override
   Widget build(BuildContext context) {
@@ -196,7 +309,8 @@ class _HomePageState extends State<HomePage> {
             const HeaderHomeWidget(),
             panelControl(),
             lightIntensityMonitor(),
-            temperatureHumidity()
+            temperatureHumidity(),
+            saveButton()
           ],
         ),
       ),
@@ -257,4 +371,10 @@ class _BlowerCardState extends State<BlowerCard> {
       ],
     );
   }
+}
+
+class ChartData {
+  ChartData(this.x, this.y);
+  final String x;
+  final double y;
 }
