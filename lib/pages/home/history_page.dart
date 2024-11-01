b933f542256c5232f4ccc9faba93f167a5d1dac6
Pramudya <pramudyap333@gmail.com>
2024-11-01 22:00:51 +0700

    update history page

--- lib/pages/home/history_page.dart
+++ lib/pages/home/history_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:potato_apps/theme.dart';
+import 'package:potato_apps/widget/button_green.dart';
 
 class HistoryPage extends StatefulWidget {
   const HistoryPage({super.key});
@@ -45,7 +47,8 @@ class _HistoryPageState extends State<HistoryPage> {
           child: SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: DataTable(
-              headingRowColor: MaterialStateColor.resolveWith((states) => primaryColor),
+              headingRowColor:
+                  MaterialStateColor.resolveWith((states) => primaryColor),
               columnSpacing: 20,
               columns: [
                 DataColumn(label: Text('No', style: whiteTextStyle)),
@@ -100,13 +103,15 @@ class _HistoryPageState extends State<HistoryPage> {
             color: primaryColor,
           ),
           child: DataTable(
-            headingRowColor: MaterialStateColor.resolveWith((states) => primaryColor),
-            columnSpacing: 24,
+            headingRowColor:
+                MaterialStateColor.resolveWith((states) => primaryColor),
+            columnSpacing: 14,
             columns: [
               DataColumn(label: Text('No.', style: whiteTextStyle)),
               DataColumn(label: Text('Normal', style: whiteTextStyle)),
               DataColumn(label: Text('Rusak', style: whiteTextStyle)),
               DataColumn(label: Text('Jumlah', style: whiteTextStyle)),
+              DataColumn(label: Text('Gambar', style: whiteTextStyle)),
             ],
             rows: [
               DataRow(cells: [
@@ -114,12 +119,35 @@ class _HistoryPageState extends State<HistoryPage> {
                 DataCell(Text('50', style: whiteTextStyle)),
                 DataCell(Text('5', style: whiteTextStyle)),
                 DataCell(Text('55', style: whiteTextStyle)),
+                DataCell(Text('Lihat', style: subtitleTextStyle)),
               ]),
               DataRow(cells: [
                 DataCell(Text('2', style: whiteTextStyle)),
                 DataCell(Text('45', style: whiteTextStyle)),
                 DataCell(Text('10', style: whiteTextStyle)),
                 DataCell(Text('55', style: whiteTextStyle)),
+                DataCell(GestureDetector(
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: Text('Image Preview'),
+                          content: Image.network(
+                            'https://firebasestorage.googleapis.com/v0/b/potato-base-34d80.appspot.com/o/photo%2Fapple.jpg?alt=media&token=e3eab426-a770-4d17-ba90-fdd2dd78f894', // Replace with your image URL or path
+                            fit: BoxFit.cover,
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Text('Close'),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    child: Text('Lihat', style: subtitleTextStyle))),
               ]),
             ],
           ),
