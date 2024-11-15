6a760ecb1637214626c9b792768492beeffcb42b
Pramudya <pramudyap333@gmail.com>
2024-11-15 14:52:01 +0700

    update appconstant

--- lib/configuration/app_constant.dart
+++ lib/configuration/app_constant.dart
@@ -7,6 +7,12 @@ class AppConstant {
   static const String userCollection = "users";
   static const String deviceCollection = "device";
 
+  // Flask API URL
+  static const String flaskURL = "http://172.16.104.219:5000";
+
+  // ESPCam URL
+  static const String espCamURL = 'http://172.16.104.219:5000/test_capture';
+
   // Google Sign-In Scopes
   static const List<String> googleScopes = [
     'email',
@@ -17,6 +23,7 @@ class AppConstant {
   static const String appName = "Potato Base";
   static const String defaultCity = "City Unknown";
   static const String imgPlaceholder = "";
+  static const String deviceID = "1730184375";
 
   // Other constants for your app (e.g., shared preferences keys, error messages, etc.)
   static const String errorSignInMessage =
