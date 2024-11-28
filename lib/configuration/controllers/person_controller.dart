d6ab90581aa231e9f950e908b32d1e02e999862c
Pramudya <pramudyap333@gmail.com>
2024-11-28 20:26:47 +0700

    add update profile image with compression file

--- lib/configuration/controllers/person_controller.dart
+++ lib/configuration/controllers/person_controller.dart
@@ -1,15 +1,23 @@
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:potato_apps/model/user_model.dart';
+import 'package:path_provider/path_provider.dart';
 
 class PersonController {
   static final googleSignIn = GoogleSignIn();
+
+  static final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  static final FirebaseStorage _storage = FirebaseStorage.instance;
 
   static Future<void> googleAuthLogin(BuildContext context) async {
     try {
@@ -197,5 +205,103 @@ class PersonController {
       print('Error updating user data: $e');
     }
   }
-  
+
+  static Future<Uint8List> testCompressFile(File file) async {
+    var result = await FlutterImageCompress.compressWithFile(
+      file.absolute.path,
+      minWidth: 2300,
+      minHeight: 1500,
+      quality: 60,
+    );
+    print(file.lengthSync());
+    print(result!.length);
+
+    return result;
+  }
+
+  static Future<String?> uploadImage(File imageFile, String userId) async {
+    try {
+      Uint8List compressed = await testCompressFile(imageFile);
+
+      File compressedFinish =
+          await uint8ListToFile(compressed, 'compressedimagefile.jpg');
+
+      // Define the storage path and file name in Firebase
+      String filePath = 'photo/profileimg/$userId.jpg';
+
+      // Upload the compressed file to Firebase Storage
+      final uploadTask = await _storage.ref(filePath).putFile(
+            compressedFinish,
+            SettableMetadata(contentType: "image/jpeg"),
+          );
+
+      // Get the download URL of the uploaded image
+      String downloadURL = await uploadTask.ref.getDownloadURL();
+
+      print("Image uploaded successfully. URL: $downloadURL");
+
+      return downloadURL;
+    } catch (e) {
+      print("Error uploading image: $e");
+      return null;
+    }
+  }
+
+  static Future<File> uint8ListToFile(Uint8List data, String fileName) async {
+    // Get the temporary directory
+    final tempDir = await getTemporaryDirectory();
+
+    // Create a full file path in the temporary directory
+    String filePath = '${tempDir.path}/$fileName';
+
+    // Write the data to the file
+    File file = File(filePath);
+    return await file.writeAsBytes(data);
+  }
+
+  static Future<bool> updateProfilePhoto(File? imgFile) async {
+    final User? currentUser = FirebaseAuth.instance.currentUser;
+    final uid = currentUser?.uid;
+
+    try {
+      // Check if an image file is provided
+      if (imgFile == null) {
+        print("No image selected.");
+        return false;
+      }
+
+      // Upload the image and get the download URL
+      String? imageUrl = await uploadImage(imgFile, uid!);
+      if (imageUrl == null) {
+        print("Failed to get image URL.");
+        return false;
+      }
+
+      // Save detection history with the image URL
+      bool success = await updateLinkImage(uid, imageUrl);
+      if (success) {
+        print("Profile saved with image URL.");
+      }
+      return success; // Return the success status of addNewDetectionHistory
+    } catch (e) {
+      print("Error in saving detection with image: $e");
+      return false; // Return false in case of an exception
+    }
+  }
+
+  static Future<bool> updateLinkImage(String userId, String linkImage) async {
+    try {
+      // Reference to the user's document
+      DocumentReference userDoc =
+          FirebaseFirestore.instance.collection('users').doc(userId);
+
+      // Update the document with the new field
+      await userDoc.update({'profile_photo': linkImage});
+      print('User data updated successfully.');
+      return true; // Indicating success
+    } catch (e) {
+      print('Error updating user data: $e');
+      return false; // Indicating failure
+    }
+  }
 }
