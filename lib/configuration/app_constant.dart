de87e63883a0a81d910dca96ea42f0ccd573a9d1
Pramudya <pramudyap333@gmail.com>
2024-12-03 21:02:51 +0700

    update variable device token

--- lib/configuration/app_constant.dart
+++ lib/configuration/app_constant.dart
@@ -8,10 +8,10 @@ class AppConstant {
   static const String deviceCollection = "device";
 
   // Flask API URL
-  static const String flaskURL = "http://192.168.1.9:5000";
+  static const String flaskURL = "http://192.168.1.5:5000";
 
   // ESPCam URL
-  static const String espCamURL = 'http://192.168.1.9:5000/test_capture';
+  static const String espCamURL = '$flaskURL/capture';
 
   // Google Sign-In Scopes
   static const List<String> googleScopes = [
@@ -19,17 +19,18 @@ class AppConstant {
     'https://www.googleapis.com/auth/userinfo.profile',
   ];
 
-
   // Application-specific constants
   static const String appName = "Potato Base";
   static const String defaultCity = "City Unknown";
   static const String imgPlaceholder = "";
   static const String deviceID = "1730184375";
   static const String deviceID2 = "1730184400";
-  
 
   // choosen device default non final value
   static String chooseDevice = AppConstant.deviceID;
+
+  // Device Token
+  static String deviceToken = "";
 
   // Other constants for your app (e.g., shared preferences keys, error messages, etc.)
   static const String errorSignInMessage =
